a25e3ac0e3f8fa42db799767854d5337e55e8883
Hao Jiang <jh991108@gmail.com>
2020-02-29 16:36:01 +1100

    third

--- hello.c
+++ hello.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-puts("hello world:yes");
+
+int main(int argc, char** argv) {
+	if (argc > 1) {
+	int n = strtol(argv[1],NULL,10);
+	int r = rand();
+	if (n ==r ) {
+	printf("win");}
+	else {
+	printf ("random %d\n",r);
+	}
+	printf("%d\n", n);
+	}
+	
+
 return 0;
 
 }
